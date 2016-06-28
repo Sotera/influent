8bf42e14cdb6cf16e6b566943a866954ef8724f2
kenglish <kevin.english@soteradefense.com>
2016-06-28 10:06:17 -0500

    Changes to get to build

--- influent-server/src/main/java/influent/server/sql/mssql/MSSQLBuilder.java
+++ influent-server/src/main/java/influent/server/sql/mssql/MSSQLBuilder.java
@@ -16,6 +16,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
+package influent.server.sql.mssql;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
